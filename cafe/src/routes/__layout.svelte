52c49d59a5f9648868ea69866178a1b07662cf59
dfg129 <dfg129@gmail.com>
2021-10-10 13:52:18 -0400

    Add layout

--- cafe/src/routes/__layout.svelte
+++ cafe/src/routes/__layout.svelte
@@ -1,14 +1,11 @@
-<script>import "../app.postcss";</script>
-<style global lang="postcss">
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-</style>
+<script lang="ts">
+    import Header from '$lib/header/Header.svelte';
+    import "../app.postcss";
+    import '../app.css';
+</script>
 
-<div class="flex flex-row">
-    <div class="bg-indigo-200 w-1/4"></div>
-    <div class="bg-red-400 text-black text-3xl text-center font-bold p-3 w-1/2">
-       Cafe Continuum
-    </div>
-    <div class="bg-green-200 w-1/4">  </div>
-</div>
+<Header/>
+
+<main>
+    <slot />
+</main>
--- cafe/src/routes/index.svelte
+++ cafe/src/routes/index.svelte
@@ -1,28 +1,11 @@
-<script>
-import Header from './Header.svelte';
-import MainNav from './navigations/MainNav.svelte';
-import Constructions from './Constructions.svelte';
-</script>
+
    
 
-<div class="flex">
-    <div class="w-1/5">    
-        <MainNav/>
+<div>
+    <div>    
+        MainNav
     </div>
-    <div class="w-4/5">
-        <Constructions/>
+    <div>
+        Constructions
     </div>
 </div>
-
-
-    <!-- <div class="flex-col">
-        <LeftNav/>
-        <Constructs>
-            <span slot="title">Abstract Title</span>
-            <div>
-                <h2>-----------------------</h2>
-            </div>
-            <span slot="body">Construct to Code</span>
-        </Constructs>
-    </div>
-</div> -->
