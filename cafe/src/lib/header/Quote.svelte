178ea2d678c17605a2ed6db9bea8d1b31d79a878
dfg129 <dfg129@gmail.com>
2021-10-11 12:42:14 -0400

    About added cleanup

--- cafe/src/lib/header/Quote.svelte
+++ cafe/src/lib/header/Quote.svelte
@@ -14,6 +14,6 @@
       }
   </style>
   
-  <h1>🎉 If you want to build a ship, don’t drum up the men to gather wood, divide the work and give orders. Instead, teach them to yearn for the vast and endless sea. </h1>
-  <h1> ~Antoine de Saint-Exupery </h1>>
+  <h1> If you want to build a ship, don’t drum up the men to gather wood, divide the work and give orders. Instead, teach them to yearn for the vast and endless sea. </h1>
+  <h1> ~Antoine de Saint-Exupery </h1>
   
